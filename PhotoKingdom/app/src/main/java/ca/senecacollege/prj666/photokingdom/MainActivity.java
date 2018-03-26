da605d03bd8f25980ddd42671478ae86d246ee94
sofimofi <sofia.ngo@gmail.com>
2018-03-25 20:22:18 -0400

    Add a different hardcoded Attractionphotowar

--- PhotoKingdom/app/src/main/java/ca/senecacollege/prj666/photokingdom/MainActivity.java
+++ PhotoKingdom/app/src/main/java/ca/senecacollege/prj666/photokingdom/MainActivity.java
@@ -168,7 +168,7 @@ public class MainActivity extends AppCompatActivity implements PhotowarFragment.
 
                 return true;
             case R.id.test_photowar_detail:
-                PhotowarFragment photowarFragment = PhotowarFragment.newInstance(9);
+                PhotowarFragment photowarFragment = PhotowarFragment.newInstance(10);
                 getSupportFragmentManager().beginTransaction()
                         .replace(R.id.frameLayout, photowarFragment )
                         .addToBackStack(null)
